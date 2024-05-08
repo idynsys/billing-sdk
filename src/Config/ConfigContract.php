65c6a95ff06fde30d603aa7223fc6e15000f3ecf
Alexander Tarkhanov <a.tarkhanov@idynsys.org>
2024-05-08 17:02:01 +0300

    правки по стилю

--- src/Config/ConfigContract.php
+++ src/Config/ConfigContract.php
@@ -4,7 +4,17 @@ namespace Idynsys\BillingSdk\Config;
 
 interface ConfigContract
 {
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return string|null
+     */
     public function get(string $key, $default = null): ?string;
 
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
     public function set(string $key, $value): void;
 }
